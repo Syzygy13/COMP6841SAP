ec9272e807ccbc61201d1428b5e98d72d22d7073
Syzygy13 <prasathjeevahan@gmail.com>
2024-11-03 01:30:16 +1100

    href fix

--- LoginTest/server.js
+++ LoginTest/server.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile("COMP6841SAP/LoginTest/public/login.html");
+    res.sendFile(path.join(initialPath, "COMP6841SAP/LoginTest/public/login.html"));
     console.log(initialPath)
 });
 
